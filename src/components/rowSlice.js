9eec9a074e46a14b9530006e83cbc836ab8d8076
Andrew Taylor <drew.j.taylor1@gmail.com>
2022-06-16 18:06:22 -0400

    Started logic for totaling a row

---
+++ src/components/rowSlice.js
@@ -0,0 +1,15 @@
+const totalMarks = 0;
+
+/**
+ * Takes in the parameter and increases it by
+ * @param {Integer} totalMarks 
+ * @returns totalMarks + 1
+ */
+export const increaseMarks = (totalMarks) => (totalMarks++);
+
+/**
+ * 
+ * @param {Integer} totalMarks 
+ * @returns 
+ */
+export const decreaseMarks = (totalMarks) => (totalMarks--);
