4992e9f33229a1391b6a0b7180d10e1b1740d4c7
Andrew Taylor <drew.j.taylor1@gmail.com>
2022-06-16 18:04:37 -0400

    Set up numbers to switch to/from "X"

--- src/components/numberbox.js
+++ src/components/numberbox.js
@@ -1,7 +1,21 @@
-const NumberBox = (props) => {
+import {useState} from 'react';
+
+const NumberBox = ({num}) => {
+    // useState to capture changes in clicks on box
+    const [currentNum, setNum] = useState(num);
+
+    // toggleX changes 'num' to and from x using the function set up with "useState"
+    const toggleX = () => {
+        if (currentNum === 'X') {
+           setNum(num)
+        } else {
+            setNum('X')
+        }
+    }
+
     return (
-        <button className="number-box m-2 btn-primary text-light border rounded text-center">
-            {props.num}
+        <button onClick={() => toggleX()} className="number-box m-2 btn-primary text-light border rounded text-center">
+            {currentNum}
         </button>
     )
 };
