c05448bde6f4de3e0691b36676eec791951bc0c6
Andrew Taylor <drew.j.taylor1@gmail.com>
2022-06-21 18:30:17 -0400

    Scoring logic/state added

--- src/components/totalbox.js
+++ src/components/totalbox.js
@@ -1,17 +1,37 @@
 import { useState } from 'react';
 
-const TotalBox = () => {
-    const [totalForRow, setRowTotal] = useState(0);
+// TotalBox has a prop of totalMarks
 
+const TotalBox = ({ totalMarks }) => {
+
+    const [scoreForRow, setRowTotal] = useState(0);
+
+    // Temporary functions set up to test scoring function
+    const calculateScoreForRow = (numberOfMarks) => {
+        let score = 0;
+        while (numberOfMarks > 0) {
+            score += numberOfMarks;
+            numberOfMarks--
+        }
+        return score
+    }
 
     return (
-        <>  
+        <>
             <button >
-                {totalForRow}
+                {calculateScoreForRow(totalMarks)}
             </button>
         </>
-        
     )
 };
 
 export default TotalBox;
+
+function scorerow(numberofmarks) {
+    let score = 0;
+    while (numberofmarks > 0) {
+        score += numberofmarks;
+        numberofmarks--;
+    };
+    return score
+}
--- src/features/qwixx/score.js
+++ src/features/qwixx/score.js
@@ -3,27 +3,29 @@ let row2marks = 2;
 let row3marks = 8;
 let row4marks = 4;
 
-let row1score = scorerow(row1marks);
-let row2score = scorerow(row2marks);
-let row3score = scorerow(row3marks);
-let row4score = scorerow(row4marks);
+let row1score = scoreRow(row1marks);
+let row2score = scoreRow(row2marks);
+let row3score = scoreRow(row3marks);
+let row4score = scoreRow(row4marks);
 
-function scorerow(numberofmarks) {
+const scoreRow = (numberofmarks) => {
     let score = 0;
     while (numberofmarks > 0) {
         score += numberofmarks;
         numberofmarks--;
     };
     return score
-}
+};
 
-function totalscore(row1score, row2score, row3score, row4score, penalties=0) {
+function totalscore(row1score, row2score, row3score, row4score, penalties = 0) {
     return row1score + row2score + row3score + row4score - penalties
-}
+};
 
-console.log(scorerow(row1marks));
-console.log(scorerow(row2marks));
-console.log(scorerow(row3marks));
-console.log(scorerow(row4marks));
+console.log(scoreRow(row1marks));
+console.log(scoreRow(row2marks));
+console.log(scoreRow(row3marks));
+console.log(scoreRow(row4marks));
 
-console.log(totalscore(row1score, row2score, row3score, row4score, 5))
+console.log(totalscore(row1score, row2score, row3score, row4score, 5));
+
+export default scoreRow
